d35f1fcae002f21c87873544b009af535b5d1264
BrentGruber <brent.gruber77@gmail.com>
2018-05-31 20:49:19 -0400

    adding model for profile info

--- bio/models.py
+++ bio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=45)
+    position = models.CharField(max_length=45)
+    location = models.CharField(max_length=45)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=45)
+    about = models.TextField()
+    links = JSONField()
+    skills = JSONField()
